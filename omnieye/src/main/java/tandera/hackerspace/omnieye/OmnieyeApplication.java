c88607235e8d9ed44b37e26c2e21aade87ea854d
Tandera, eye of <tandera@hackerspace.com>
2023-08-10 16:03:40 -0300

    Spring project structure

---
+++ omnieye/src/main/java/tandera/hackerspace/omnieye/OmnieyeApplication.java
@@ -0,0 +1,13 @@
+package tandera.hackerspace.omnieye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnieyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnieyeApplication.class, args);
+	}
+
+}
---
+++ omnieye/src/test/java/tandera/hackerspace/omnieye/OmnieyeApplicationTests.java
@@ -0,0 +1,13 @@
+package tandera.hackerspace.omnieye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnieyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
